e4eb85b043fee7c95ad845f185ede66bae784251
Adrian Hooper <adrian@euphoricpanda.co.uk>
2016-11-14 13:09:23 +0000

    Bumped version ready for release

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Geonames.Mixfile do
 
   def project do
     [app: :geonames,
-     version: "1.0.0",
+     version: "1.0.1",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
