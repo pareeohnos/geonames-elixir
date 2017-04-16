e3b52c3d54f1c869ce06fe9942e900636dc60f64
Adrian Hooper <adrian@euphoricpanda.co.uk>
2017-04-16 11:31:46 +0100

    Prep travis-ci configuration, and altered elixir requirement

--- mix.exs
+++ mix.exs
@@ -3,8 +3,8 @@ defmodule Geonames.Mixfile do
 
   def project do
     [app: :geonames,
-     version: "1.0.1",
-     elixir: "~> 1.2",
+     version: "1.0.2",
+     elixir: ">= 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps,
